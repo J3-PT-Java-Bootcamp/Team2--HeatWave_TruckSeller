d74db38850be8f16ada5df3ab024060c261ccc2c
Pau <pauescola12@gmail.com>
2022-07-17 21:56:23 +0200

    Estructura básica creada
    Classes creades: Screens, BootCRM.
    
    Implementada primera funcionalitat de menus, login (amb check credentials)
    
    Falta testejar però té bona pinta.

--- src/main/java/com/ironhack/Commercial/Lead.java
+++ src/main/java/com/ironhack/Commercial/Lead.java
@@ -1,4 +1,58 @@
 package com.ironhack.Commercial;
 
 public class Lead {
+
+    private String name;
+    private String id;
+    private String phoneNumber;
+    private String eMail;
+    private String companyName;
+
+    public Lead(String name, String id, String phoneNumber, String eMail, String companyName) {
+        setName(name);
+        setId(id);
+        setPhoneNumber(phoneNumber);
+        seteMail(eMail);
+        setCompanyName(companyName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 }
---
+++ src/main/java/com/ironhack/MenuUtilities/MenuOption.java
@@ -0,0 +1,52 @@
+package com.ironhack.MenuUtilities;
+
+public class MenuOption<T> {
+    private String display;
+    private T value;
+    private boolean available = true;
+
+    public MenuOption(String display, T value) {
+        this.display = display;
+        this.value = value;
+    }
+    public MenuOption(String display, T value, boolean available) {
+        this.display = display;
+        this.value = value;
+        this.available = available;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public void display(int index) {
+        System.out.println("(" + (index + 1) + ") " + getDisplay() + (isAvailable() ? "" : " [DISABLED]"));
+    }
+
+    public static <T> MenuOption<T> create(String display, T value) {
+        return new MenuOption<T>(display, value);
+    }
+    public static <T> MenuOption<T> create(String display, T value, boolean available) {
+        return new MenuOption<T>(display, value, available);
+    }
+}
