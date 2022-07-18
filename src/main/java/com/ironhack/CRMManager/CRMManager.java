c2506e04eab0334b4643e668376adf0974d38203
VladScv <ddcllrnsbrv@uoc.edu>
2022-07-18 14:44:27 +0200

    getNextID() created

--- src/main/java/com/ironhack/CRMManager/CRMManager.java
+++ src/main/java/com/ironhack/CRMManager/CRMManager.java
@@ -1,18 +1,20 @@
 package com.ironhack.CRMManager;
 
+import com.ironhack.Commercial.Account;
+import com.ironhack.Commercial.Contact;
 import com.ironhack.Commercial.Lead;
 import com.ironhack.Commercial.Opportunity;
 
 import java.util.HashMap;
 
+import static com.ironhack.Constants.Constants.MAX_ID;
+
 public class CRMManager {
 
-    private int leadCounter,opportunityCounter;
+    private int leadCounter,opportunityCounter,accountCounter,contactCounter;
     private HashMap<String, Lead> leadList;
     private HashMap<String, Opportunity> opportunityList;
     private HashMap<String, User> userList;
-
-
 
 
     public CRMManager(){
@@ -27,6 +29,23 @@ public class CRMManager {
         }
     }
 
+    public String getNextID(Class objClass){
+        var sb= new StringBuilder();
+        if (Opportunity.class.equals(objClass)) {
+            opportunityCounter++;
+            sb.append("OP").append(Integer.toHexString(Integer.parseInt(MAX_ID, 16) - opportunityCounter));
+        } else if (Lead.class.equals(objClass)) {
+            leadCounter++;
+            sb.append("L").append(Integer.toHexString(Integer.parseInt(MAX_ID, 16) - leadCounter));
+        } else if (Account.class.equals(objClass)) {
+            accountCounter++;
+            sb.append("A").append(Integer.toHexString(Integer.parseInt(MAX_ID, 16) - accountCounter));
+        } else if (Contact.class.equals(objClass)) {
+            contactCounter++;
+            sb.append("C").append(Integer.toHexString(Integer.parseInt(MAX_ID, 16) - contactCounter));
+        }
+        return sb.toString();
+    }
     private void loadData() throws Exception {
         //TODO
         throw new IllegalAccessException();
