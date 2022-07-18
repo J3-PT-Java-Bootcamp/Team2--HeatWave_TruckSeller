9d1ab84321e32599ddfc35218e4cdbc973f7955b
VladScv <ddcllrnsbrv@uoc.edu>
2022-07-18 19:19:54 +0200

    CRMScreen linkedScreens

--- src/main/java/com/ironhack/ScreenManager/Screens/CRMScreen.java
+++ src/main/java/com/ironhack/ScreenManager/Screens/CRMScreen.java
@@ -8,9 +8,13 @@ import static com.ironhack.Constants.Constants.*;
 
 public abstract class CRMScreen {
     private WindowObject textObject;
+    private String name;
+    private java.util.HashMap<String,CRMScreen> linkedScreens;
     private CRMManager crmManager;
 
-    public CRMScreen(String name){
+    public CRMScreen(String name,CRMScreen... linkedScreens){
+        this.linkedScreens =new java.util.HashMap<>();
+        for(CRMScreen screen :linkedScreens)this.linkedScreens.put(screen.getName(),screen);
         this.textObject=new WindowObject(LIMIT_X,LIMIT_Y,1,0)
                 .setBgColor(BgColors.WHITE)
                 .setFrameColor(BgColors.WHITE)
@@ -18,6 +22,11 @@ public abstract class CRMScreen {
                 .setTitle(name)
                 .setTitleColor(CColors.BRIGHT_BLACK);
     }
+
+    public String getName() {
+        return this.name;
+    }
+
     public abstract TextObject constructScreen();
     public abstract void getInput();
 
