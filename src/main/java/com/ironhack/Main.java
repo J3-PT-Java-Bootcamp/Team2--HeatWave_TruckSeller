5a3dce98b2743188888e7a07812bf085049da56a
VladScv <ddcllrnsbrv@uoc.edu>
2022-07-20 18:25:03 +0200

    Test Testing

--- src/main/java/com/ironhack/Main.java
+++ src/main/java/com/ironhack/Main.java
@@ -2,6 +2,6 @@ package com.ironhack;
 
 public class Main {
     public static void main(String[] args) {
-        com.ironhack.CRMManager.CRMManager manager = new com.ironhack.CRMManager.CRMManager(true);
+        com.ironhack.CRMManager.CRMManager manager = new com.ironhack.CRMManager.CRMManager();
     }
 }
