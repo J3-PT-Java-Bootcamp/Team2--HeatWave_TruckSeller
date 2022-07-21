8e2766b1ab52ec57dc22253fa28d800918780e8d
VladScv <ddcllrnsbrv@uoc.edu>
2022-07-21 23:26:29 +0200

    TableScreen implementation

--- src/test/java/com/ironhack/Commercial/OpportunityTest.java
+++ src/test/java/com/ironhack/Commercial/OpportunityTest.java
@@ -1,13 +1,20 @@
 package com.ironhack.Commercial;
 
+import com.ironhack.CRMManager.CRMData;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpportunityTest extends com.ironhack.ScreenManager.Text.TextObject {
+    static CRMData crm;
+    @org.junit.jupiter.api.BeforeAll
+    static void setUp() {
+         crm = new CRMData();
+    }
 
     @org.junit.jupiter.api.Test
     void getId_test() {
-        var crm = new com.ironhack.CRMManager.CRMData();
-        var opp= new Opportunity( crm,null,1,null,null,null);
+        String nextID = crm.getNextID(Opportunity.class);
+        var opp= new Opportunity(nextID,null,1,null,null,null);
         System.out.println(opp.getId());
         assertEquals("OPFFE",opp.getId());
     }
