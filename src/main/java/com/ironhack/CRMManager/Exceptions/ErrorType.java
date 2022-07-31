3fbab8c9e22a1c9ecb57bafeac8d4437f7c06f4d
abigailcfreeman <abigailcfreemandev@gmail.com>
2022-07-31 22:38:49 +0200

    Error Type to string

--- src/main/java/com/ironhack/CRMManager/Exceptions/ErrorType.java
+++ src/main/java/com/ironhack/CRMManager/Exceptions/ErrorType.java
@@ -1,31 +1,27 @@
-package com.ironhack.CRMManager.Exceptions;
+package com.ironhack.Exceptions;
 
 public enum ErrorType {
-    OK,
-    FORMAT_NOK,
-    INTEGER_NOK,
-    ID_NOK,
-    LENGTH_NOK,
-    FATAL_ERR,
-    MAIL_NOK,
-    PHONE_NOK,
-    PRODUCT_NOK,
+    OK("All good!"),
+    FORMAT_NOK("The format is not okay, please try a different format"),
+    INTEGER_NOK("The input is not correct, please input an integer"),
+    ID_NOK("The ID is not correct"),
+    LENGTH_NOK("The length is not correct"),
+    FATAL_ERR("The has been a fatal error"),
+    MAIL_NOK("The email is not correct"),
+    PHONE_NOK("The phone is not correct"),
+    COMMAND_NOK("The command is not correct"),
+    EXIT("Exit"),
+    PASSWORD_NOK("Unfortunately, the password is not correct"),
+    WRONG_PASSWORD("Unfortunately, the password is not correct"),
+    HELP("Enter HELP to receive further instructions"),
+    INDUSTRY_NOK("The industry type entered is not correct");
 
-    COMMAND_NOK, EXIT, PASSWORD_NOK, WRONG_PASSWORD,HELP, INDUSTRY_NOK;
+    ErrorType(String s) {
 
-
-    private String printName;
-
-    ErrorType(String printName){
-        this.printName=printName;
-    }
-
-    ErrorType() {
     }
 
     @Override
     public String toString() {
         return this.name();
     }
-
 }
