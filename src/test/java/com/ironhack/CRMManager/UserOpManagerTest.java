4c0820579884d859a3862f1880568f8e3e640381
VladScv <ddcllrnsbrv@uoc.edu>
2022-08-02 14:04:07 +0200

    All tests passed

--- src/test/java/com/ironhack/CRMManager/UserOpManagerTest.java
+++ src/test/java/com/ironhack/CRMManager/UserOpManagerTest.java
@@ -1,5 +1,6 @@
 package com.ironhack.CRMManager;
 
+import com.ironhack.CRMManager.Exceptions.GoBackException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +10,7 @@ import java.io.ByteArrayInputStream;
 import static com.ironhack.CRMManager.CRMManager.crmData;
 import static com.ironhack.CRMManager.CRMManager.userOpManager;
 import static com.ironhack.Constants.OpportunityStatus.CLOSE_WON;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserOpManagerTest {
 
@@ -25,22 +26,28 @@ class UserOpManagerTest {
     }
 
     @Test
-    void closeOpportunity() {
-        System.setIn(new ByteArrayInputStream("HELP\n".getBytes()));
+    void closeOpportunity_test_ok() {
          userOpManager.closeOpportunity(crm.getCurrentUser(),new String[]{"CLOSE","WON","PFFE"});
          assertEquals(CLOSE_WON,crmData.getOpportunity("PFFE").getStatus());
     }
 
     @Test
-    void createNewAccount() {
+    void createNewAccount_exceptionTest_ok()  {
+        System.setIn(new ByteArrayInputStream("back\n".getBytes()));
+        assertThrows(GoBackException.class,()->userOpManager.createNewAccount(crm.getCurrentUser()));
 
     }
 
     @Test
-    void convertLeadToOpp() {
+    void convertLeadToOpp_exceptionTest_ok() {
+        System.setIn(new ByteArrayInputStream("menu\n".getBytes()));
+        assertDoesNotThrow(()->userOpManager.convertLeadToOpp(crmData.getUser("USER"),new String[]{"CONVERT","LFFE"}));
     }
 
     @Test
-    void viewObject() {
+    void viewObject_exceptionTest_ok() {
+        System.setIn(new ByteArrayInputStream("back\n".getBytes()));
+        assertThrows(GoBackException.class,()->userOpManager.viewObject(crm.getCurrentUser(),
+                new String[]{"VIEW","LFFE"}));
     }
 }
