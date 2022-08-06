0e31fb36e4803f72c8f8dc1b3cd2f5c50ef37984
BeaIbanez <bea.ibanez.perez@gmail.com>
2022-08-06 09:43:17 +0200

    last commit

--- src/test/java/com/ironhack/Sales/AccountTest.java
+++ src/test/java/com/ironhack/Sales/AccountTest.java
@@ -1,23 +1,50 @@
 package com.ironhack.Sales;
 
 import com.ironhack.CRMManager.CRMManager;
+import com.ironhack.CRMManager.ScreenManager.Text.TextObject;
 import com.ironhack.Constants.IndustryType;
+import com.ironhack.Constants.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.ironhack.CRMManager.CRMManager.crmData;
+import static com.ironhack.CRMManager.ScreenManager.InputReader.toCamelCase;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+    CRMManager crm;
+    Account acc;
     @BeforeEach
     void setUp() {
-        new CRMManager(false);
-
+        crm = new CRMManager(false);
+        acc= new Account(IndustryType.MEDICAL, 30,"Orlando","Pakistan", "Reebok");
+        crmData.addAccount(acc);
     }
 
     @Test
     void getCompanyName_test() {
         var acc= new Account(IndustryType.MEDICAL,39,"BCN","Spain","CocaCola");
         assertEquals("CocaCola",acc.getCompanyName());
+    }
+
+    @Test
+    void test_textObject(){
+        assertEquals(5,acc.toTextObject().getTotalHeight() );
+    }
+
+    @Test
+    void test_String(){
+        assertNotNull(acc.shortPrint());
+    }
+
+    @Test
+    void test_printFullObject(){
+        assertEquals(5,acc.printFullObject().getTotalHeight() );
+    }
+
+    @Test
+    void test_getPrintableAttributes(){
+        assertEquals(acc.toTextObject().getTotalHeight(),acc.getPrintableAttributes().length );
     }
 
 }
--- src/test/java/com/ironhack/Sales/LeadTest.java
+++ src/test/java/com/ironhack/Sales/LeadTest.java
@@ -1,15 +1,24 @@
 package com.ironhack.Sales;
 
 import com.ironhack.CRMManager.CRMManager;
+import com.ironhack.CRMManager.ScreenManager.Text.TextObject;
+import com.ironhack.Constants.IndustryType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.ironhack.CRMManager.CRMManager.crmData;
+import static com.ironhack.CRMManager.ScreenManager.InputReader.MAIL;
+import static com.ironhack.CRMManager.ScreenManager.InputReader.toCamelCase;
+import static org.junit.jupiter.api.Assertions.*;
 
 class LeadTest {
+    CRMManager crm;
+    Lead led;
     @BeforeEach
     void setUp() {
-        new CRMManager(false);
+        crm = new CRMManager(false);
+        led= new Lead("bea","+34","92526272","Adidas","Ships");
+        crmData.addLead(led);
 
     }
     @Test
@@ -18,7 +27,26 @@ class LeadTest {
         assertEquals("1234",led.getId());
     }
 
+    @Test
+    void test_textObject(){
+        assertEquals(5,led.toTextObject().getTotalHeight() );
+    }
 
+    @Test
+    void test_String(){
+        assertNotNull(led.shortPrint());
+    }
 
+    @Test
+    void test_printFullObject(){
+        assertEquals(5,led.printFullObject().getTotalHeight() );
+    }
+
+    @Test
+    void test_getPrintableAttributes(){
+        assertEquals(led.toTextObject().getTotalHeight(),led.getPrintableAttributes().length );
+    }
 
 }
+
+
